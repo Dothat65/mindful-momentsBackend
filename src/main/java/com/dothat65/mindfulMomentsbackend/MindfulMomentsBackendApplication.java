811b60ce99a1e4d320b47806636d6f4c1e451f72
Dothat65 <meemoahmed2004@gmail.com>
2024-05-02 23:30:47 -0400

    First commit

---
+++ src/main/java/com/dothat65/mindfulMomentsbackend/MindfulMomentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dothat65.mindfulMomentsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindfulMomentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindfulMomentsBackendApplication.class, args);
+	}
+
+}
